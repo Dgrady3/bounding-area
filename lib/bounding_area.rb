abd9ff84a9bbfc4e4f4163594af98a4c99986bdc
Joe Grady <jogrady1@gmail.com>
2014-09-06 16:23:31 -0400

    first challenge pass

---
+++ lib/bounding_area.rb
@@ -0,0 +1,15 @@
+class BoundingArea
+  def initialize(box_array)
+    @box_array = []
+  end
+
+  def contains_point?(x, y)
+    if x == 0.0 && y == 0.0
+      false
+    end
+  end
+
+end
+
+
+
---
+++ lib/bounding_box.rb
@@ -0,0 +1,9 @@
+class BoundingBox
+
+  def initialize (left_edge,right_edge, bottom_edge, top_edge)
+    @left_edge = left_edge
+    @right_edge = right_edge
+    @bottom_edge = bottom_edge
+    @top_edge = top_edge
+  end
+end
